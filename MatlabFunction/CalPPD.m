3782efa434a4ddf92054852d1e2476b73e4eb4b7
iyunsu <r07525010@ntu.edu.tw>
2021-06-08 00:16:39 +0800

    Add CalPPD.m

--- MatlabFunction/CalPPD.m
+++ MatlabFunction/CalPPD.m
@@ -6,25 +6,88 @@
 %       Calculate the PPD from all detected points.
 %% ========================================================================
 clear all;clc
+%% Set Param
+FLPoint = [121.2 22.08];
+GuessPoint = [121.19 22.065];
+desireDepth = '5';
+sumPPD = zeros(1192,1192);
+iDivisor = zeros(1192,1192);
+
 %% Set Path
 path_output = '/home/iyunsu/gbm/Example_121.2_22.08/GbmSearchPoints/37500';
-cd(path_output)
-detectedPoints = dir('*_5m')
+path_detected = '/home/iyunsu/gbm/Example_121.2_22.08/FRLocation/37500_Corr';
 
-for i = 1:length(detectedPoints)
+cd(path_output)
+detectedPointsFolder = dir(['*_',desireDepth,'m']);
+
+%% Get Detected Points
+cd(path_detected)
+detectedPointsFile = dir(['PD',desireDepth,'results.mat']);
+load(detectedPointsFile.name)
+
+%% For Loop - all points
+cd(path_output)
+
+for i = 1:length(detectedPointsFolder)
     %% Get lat amd lon from folder name
-    cd (detectedPoints(i).name)
-    
+    cd (detectedPointsFolder(i).name)
     
     %% load gray jpg
+    jpg = flipud(im2double(rgb2gray(imread('PD5mGray.jpg'))));
+    jpg = jpg(4:end,1:end-3);
+    jpg = 1-jpg;
     
     %% Average the data
     
-    
+    sumPPD = sumPPD + jpg;
+    isNotZero = find(jpg~=0);
+    iDivisor(isNotZero) = iDivisor(isNotZero) +1;
     
     cd ..
 end
+%%
+PPD = sumPPD./iDivisor;
+xPoints = [GuessPoint(1)-0.045: 0.09/(length(PPD)-1):GuessPoint(1)+0.045];
+yPoints = [GuessPoint(2)-0.045: 0.09/(length(PPD)-1):GuessPoint(2)+0.045]';
 
-%% Output
+r = pcolor(xPoints,yPoints,PPD);
+r.LineStyle = 'none';
+hold on
+dp = plot(xdetected,ydetected,'b^','MarkerSize',10)
+frp = plot(FLPoint(1),FLPoint(2),'s','MarkerEdgeColor','w','MarkerFaceColor','k','MarkerSize',15,'LineWidth',2)
+gp = plot(GuessPoint(1),GuessPoint(2),'o','MarkerEdgeColor','w','MarkerFaceColor','r','MarkerSize',15,'LineWidth',3)
 
+c = colorbar
+colormap(jet)
+caxis([0 1])
+legend([frp,gp,dp],{'Flight Recorder','Guess Point','Received signal points'})
 
+c.Label.String = 'Predictive Probability of Detection'
+c.Label.Rotation = 90
+xlabel('Longitude (˚E)');
+ylabel('Latitude (˚N)');
+title({['Predictive Probability of Detection Results'],['Received Depth = ',desireDepth,' m']})
+
+%% Save plot
+cd(path_output)
+saveas(gcf, ['PPDresults',desireDepth,'m.jpg'])
+
+%% Zoom in (Manual)
+PPDhp = zeros(1192,1192);
+PPDhp(PPD>0.75) = 1;
+
+figure(2)
+f2 = pcolor(xPoints,yPoints,PPDhp);
+f2.LineStyle = 'none';
+[xMatrix, yMatrix] = meshgrid(xPoints,yPoints);
+ii = find(PPDhp == 1)
+lonHp = xMatrix(ii);
+latHp = yMatrix(ii);
+lonRange = [min(min(lonHp)) max(max(lonHp))];
+latRange = [min(min(latHp)) max(max(latHp))];
+
+searchPoints2ndlat = (min(min(latHp)):0.002:max(max(latHp)))';
+searchPoints2ndlon = (min(min(lonHp)):0.002:max(max(lonHp)));
+[searchPoints2ndlonMatrix, searchPoints2ndlatMatrix] = meshgrid(searchPoints2ndlon,searchPoints2ndlat);
+
+interp2()
