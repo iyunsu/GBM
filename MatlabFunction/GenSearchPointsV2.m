bb4c5028b8176824a8c796ee3b1e3e9fe1632bb2
iyunsu <r07525010@ntu.edu.tw>
2021-06-14 02:40:11 +0800

    Save detected points (per 200m)

--- MatlabFunction/GenSearchPointsV2.m
+++ MatlabFunction/GenSearchPointsV2.m
@@ -10,23 +10,25 @@ clear all;clc
 %% Set Path
 path_input = '/home/iyunsu/gbm/Example_121.2_22.08/GbmSearchPoints/37500';
 path_output = '/home/iyunsu/gbm/Example_121.2_22.08/GbmSearchPointsV2/37500';
-desireDepth = '5';
+zrDepth = '5';
 cd(path_input)
-load(['PPDresults',desireDepth,'mPer10m.mat'])
+load(['PPDresults',zrDepth,'mPer10m.mat'])
 %% Average Filter
-windoeSize = 10;
+windoeSize = 20;
 averageWindow = fspecial('average',[windoeSize,windoeSize]);
-PPDper10mAfter100mAve = imfilter(PPDper10m,averageWindow);
-PPDper100m = PPDper10mAfter100mAve(1:windoeSize:end,1:windoeSize:end);
-latGrid100m = latGrid(1:windoeSize:end,1:windoeSize:end);
-lonGrid100m = lonGrid(1:windoeSize:end,1:windoeSize:end);
+PPDper10mAfter200mAve = imfilter(PPDper10m,averageWindow);
+PPDper200m = PPDper10mAfter200mAve(1:windoeSize:end,1:windoeSize:end);
+latGrid200m = latGrid(1:windoeSize:end,1:windoeSize:end);
+lonGrid200m = lonGrid(1:windoeSize:end,1:windoeSize:end);
 %% Find PPD value over 0.75
-threshold = 0.8;
-detectedIndex = find(PPDper100m >threshold);
-searchPointsLatV2 = latGrid100m(detectedIndex);
-searchPointsLonV2 = lonGrid100m(detectedIndex);
+threshold = 0.75;
+detectedIndex = find(PPDper200m >threshold);
+searchPointsLatV2 = latGrid200m(detectedIndex);
+searchPointsLonV2 = lonGrid200m(detectedIndex);
 
-%%
-ii = pcolor(lonGrid100m,latGrid100m,PPDper100m);ii.LineStyle = 'none';
+%% Save Search Points 
+save(['2ndPD',num2str(zrDepth),'mDetectedOver',num2str(threshold),'.mat'],'searchPointsLatV2','searchPointsLonV2')
+%%   CHECK
+ii = pcolor(lonGrid200m,latGrid200m,PPDper200m);ii.LineStyle = 'none';
 hold on
 plot(searchPointsLonV2,searchPointsLatV2,'*')
